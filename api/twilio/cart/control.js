a40097f159e1ee5b5ffd81c57538184e8d0a358a
Ahron Meir <ahron@mac.lan>
2026-03-30 09:37:26 -0700

    Add explicit Vercel cart endpoints

---
+++ api/twilio/cart/control.js
@@ -0,0 +1,16 @@
+const { handleHttpRequest } = require("../../../src/ivr");
+
+module.exports = async function handler(req, res) {
+  req.url = buildRequestUrl(req);
+  await handleHttpRequest(req, res);
+};
+
+function buildRequestUrl(req) {
+  const current = new URL(String(req.url || "/api/twilio/cart/control"), "http://localhost");
+  const search = new URLSearchParams(current.search);
+  search.delete("route");
+  search.delete("...route");
+
+  const queryString = search.toString();
+  return queryString ? `/api/twilio/cart/control?${queryString}` : "/api/twilio/cart/control";
+}
