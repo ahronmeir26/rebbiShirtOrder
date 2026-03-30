c36987ab4baf6842aa65072cc50cfaabb65d746a
Ahron Meir <ahron@mac.lan>
2026-03-29 21:41:11 -0700

    Rebuild Twilio route paths on Vercel

--- api/twilio/[...route].js
+++ api/twilio/[...route].js
@@ -1,6 +1,27 @@
 const { handleHttpRequest } = require("../../src/ivr");
 
 module.exports = async function handler(req, res) {
-  req.url = req.url || "/api/twilio/voice";
+  req.url = buildRequestUrl(req);
   await handleHttpRequest(req, res);
 };
+
+function buildRequestUrl(req) {
+  const currentUrl = String(req.url || "");
+  const current = new URL(currentUrl || "/api/twilio/voice", "http://localhost");
+  const routeSegments = Array.isArray(req.query?.route)
+    ? req.query.route
+    : req.query?.route
+      ? [req.query.route]
+      : [];
+
+  if (routeSegments.length === 0) {
+    return currentUrl || "/api/twilio/voice";
+  }
+
+  const search = new URLSearchParams(current.search);
+  search.delete("route");
+
+  const queryString = search.toString();
+  const pathname = `/api/twilio/${routeSegments.join("/")}`;
+  return queryString ? `${pathname}?${queryString}` : pathname;
+}
